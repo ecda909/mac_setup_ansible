98b33762b020a64212b06f391f0b31bf56ed0309
Eder Mazariegos <Ecda909@gmail.com>
2022-02-22 10:21:16 -0500

    main - Made Changes During Test On New Mac Setup

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
+# install python3, Ansible and get pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
-sudo pip install ansible
-sudo pip install awscli  
+sudo python3 get-pip.py
+sudo pip3 install ansible
 
+# Run the Ansible Playbook
 ansible-playbook -i inventory.ini main.yml --ask-become-pass
-
